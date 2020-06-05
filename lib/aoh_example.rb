7b08edcbda707c60cf1dcabce59c7bcf726415e7
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-05 16:54:14 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,13 +11,14 @@ def assembled_aoh
 end
 
 def literal_aoh
- 
- [ name:  "Bruce Green", occupation: "Fan of Mildred",
-   name:  "Don Gately", occupation: "Live-in Staff",
-   name:  "Joelle van Dyne", occupation: "Radio Personality",
-   name:  "Kate Gompert", occupation: "None",
-   name:  "Pat Monteseian", occupation: "Staff"
-   ]
+  [
+     { name:  "Don Gately", occupation: "Live-in Staff" },
+     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+     { name:  "Pat Monteseian", occupation: "Staff" },
+     { name:  "Kate Gompert", occupation: "None" },
+     { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ].sort
+  
 end
 
 def aoh_lookup(aoh, row, key)
