090301360318551031a9b3391763e22b7e9fddf1
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-05 16:49:21 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,12 @@ def assembled_aoh
 end
 
 def literal_aoh
- assembled_aoh.sort
+ [ name:  "Don Gately", occupation: "Live-in Staff",
+   name:  "Joelle van Dyne", occupation: "Radio Personality",
+   name:  "Pat Monteseian", occupation: "Staff",
+   name:  "Kate Gompert", occupation: "None",
+   name:  "Bruce Green", occupation: "Fan of Mildred"
+   ].sort
 end
 
 def aoh_lookup(aoh, row, key)
